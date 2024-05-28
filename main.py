a5de09797759dc998914a7e854c187cb640e1214
Gill-Singh-A <amansg22@iitk.ac.in>
2024-05-29 02:35:10 +0530

    Implemented Function to Create a Github Repository

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 #! /usr/bin/env python3
 
+import requests, json
+
+githubAPI = "https://api.github.com/user/repos"
+
+def createRepository(auth_token, name, visibility):
+    headers = {
+        "Authorization": f"token {auth_token}"
+    }
+    data = {
+        "name": name,
+        "private": visibility,
+    }
+    response = requests.post(githubAPI, headers=headers, data=json.dumps(data))
+    if response.status_code == 201:
+        return True
+    return response.status_code
+
+
 if __name__ == "__main__":
     pass
