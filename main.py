67dc715581ce011c1fd1e89a6068bbd7f402cd62
Gill-Singh-A <amansg22@iitk.ac.in>
2024-05-29 04:02:42 +0530

    Implemented Storing of Github API Authentication Token with AES-256 Encryption

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #! /usr/bin/env python3
 
-import requests, json, os, sys
+import requests, json, os, sys, pickle
+from getpass import getpass
 from pathlib import Path
 from datetime import date
 from optparse import OptionParser
 from colorama import Fore, Back, Style
 from time import strftime, localtime, sleep, time
+
+from aes_256 import encrypt, decrypt, generate_key
 
 status_color = {
     '+': Fore.GREEN,
@@ -59,3 +62,35 @@ def makeFolders():
 
 if __name__ == "__main__":
     makeFolders()
+    arguments = get_arguments(('-u', "--user", "user", "Github Username"))
+    if not arguments.user:
+        display('-', f"Please Provide a Username")
+        exit(0)
+    try:
+        with open("authentication_tokens.pickle", 'rb') as file:
+            authentication_tokens = pickle.load(file)
+    except FileNotFoundError:
+        authentication_tokens = {}
+    except:
+        display('-', f"Error while Reading Authentication Tokens")
+        exit(0)
+    if arguments.user not in authentication_tokens.keys():
+        auth_token = input(f"Enter Github API Authentication Token for {arguments.user} : ")
+        auth_storing_status = input(f"Do you want to Store the Token (Yes/No) : ")
+        if auth_storing_status == "Yes":
+            auth_token_password = getpass(f"Enter the Password to Securely Store the Token : ")
+            key, salt = generate_key(auth_token_password)
+            encrypted_auth_token = encrypt(auth_token.encode(), key, salt)
+            authentication_tokens[arguments.user] = {"token": encrypted_auth_token, "salt": salt}
+            with open("authentication_tokens.pickle", 'wb') as file:
+                pickle.dump(authentication_tokens, file)
+    else:
+        encrypted_auth_token = authentication_tokens[arguments.user]["token"]
+        salt = authentication_tokens[arguments.user]["salt"]
+        auth_token_password =  getpass(f"Enter the Password for Accessing Token : ")
+        try:
+            key, _ = generate_key(auth_token_password, salt)
+            auth_token = decrypt(encrypted_auth_token, key, salt).decode()
+        except Exception as err:
+            display('-', f"Wrong Password!")
+            exit(0)
