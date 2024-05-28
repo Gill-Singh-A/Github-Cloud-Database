6522300124e7703fe931a41067cbfba69bf0f1db
Gill-Singh-A <amansg22@iitk.ac.in>
2024-05-29 02:37:34 +0530

    Implemented Function to Delete a Github Repository

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import requests, json
 
-githubAPI = "https://api.github.com/user/repos"
+githubREPO_API = "https://api.github.com/user/repos"
 
 def createRepository(auth_token, name, visibility):
     headers = {
@@ -12,10 +12,18 @@ def createRepository(auth_token, name, visibility):
         "name": name,
         "private": visibility,
     }
-    response = requests.post(githubAPI, headers=headers, data=json.dumps(data))
+    response = requests.post(githubREPO_API, headers=headers, data=json.dumps(data))
     if response.status_code == 201:
         return True
-    return response.status_code
+    return response
+def deleteRepository(auth_token, user, repository):
+    headers = {
+        "Authorization": f"token {auth_token}"
+    }
+    response = requests.delete(f"https://api.github.com/repos/{user}/{repository}", headers=headers)
+    if response.status_code == 204:
+        return True
+    return response
 
 
 if __name__ == "__main__":
