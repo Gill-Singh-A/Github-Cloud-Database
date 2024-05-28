7cddb19b23503bc6e4de8daaced62f2742b436ff
Gill-Singh-A <amansg22@iitk.ac.in>
2024-05-29 02:55:11 +0530

    Implemented AES-256 Symmetric Encryption Algorithm

--- aes_256.py
+++ aes_256.py
@@ -1,4 +1,43 @@
 #! /usr/bin/env python3
 
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+
+def generate_key(password):
+    salt = os.urandom(16)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = kdf.derive(password.encode())
+    return key, salt
+
+def encrypt(plaintext, key, salt):
+    padder = padding.PKCS7(algorithms.AES.block_size).padder()
+    padded_data = padder.update(plaintext) + padder.finalize()
+    iv = os.urandom(16)
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+    return iv + salt + ciphertext
+
+def decrypt(ciphertext, key, salt):
+    iv = ciphertext[:16]
+    salt = ciphertext[16:32]
+    ciphertext = ciphertext[32:]
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    decrypted_data = decryptor.update(ciphertext) + decryptor.finalize()
+    unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
+    plaintext = unpadder.update(decrypted_data) + unpadder.finalize()
+    return plaintext
+
 if __name__ == "__main__":
     pass
